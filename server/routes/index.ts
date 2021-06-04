117518154460cad16275986004868f6240426f56
Jamee Kim <jameekim@nodaplife.me>
2021-06-03 23:23:32 -0400

    Make the nav bar look neat

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,7 +3,36 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-/* GET home page. */
-router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
-});
+const navBarLinks = [
+  {
+    url: "/",
+    title: "Home",
+    id: "home",
+  },
+  {
+    url: "/about",
+    title: "About Me",
+    id: "about",
+  },
+  {
+    url: "/projects",
+    title: "My Projects",
+    id: "projects",
+  },
+  {
+    url: "/services",
+    title: "Services",
+    id: "services",
+  },
+  {
+    url: "/contact",
+    title: "Contact Me",
+    id: "contact",
+  },
+];
+
+for (const link of navBarLinks) {
+  router.get(link.url, (req, res, next) => {
+    res.render("index", { title: link.title, page: link.id, navBarLinks });
+  });
+}
